eddd6ba2a46b28dc77b526697803c911c9537e8d
Nathan Patton <npatton@gmail.com>
2020-10-10 15:08:40 -0500

    version bump

--- djangobible/__init__.py
+++ djangobible/__init__.py
@@ -2,6 +2,6 @@
 django-bible lets you easily associate Django models with Bible Scripture references and search accordingly.
 """
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from pythonbible import *
