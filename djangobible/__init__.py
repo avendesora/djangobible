47f7ef301e57d48d65a3769f084009c1febdba7e
Nathan Patton <npatton@gmail.com>
2020-10-15 16:05:09 -0500

    update dependency requirements for pythonbible for a couple of bug fixes and bump the djangobible version up as well

--- djangobible/__init__.py
+++ djangobible/__init__.py
@@ -2,6 +2,6 @@
 django-bible lets you easily associate Django models with Bible Scripture references and search accordingly.
 """
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 from pythonbible import *
