ec17c2e512ceb6a74506e1515d9cdaf118137126
Nathan Patton <npatton@gmail.com>
2020-10-27 13:24:42 -0500

    version number bump for optional dependencies

--- djangobible/__init__.py
+++ djangobible/__init__.py
@@ -4,6 +4,6 @@ django-bible python library.
 django-bible lets you easily associate Django models with Bible Scripture references and search accordingly.
 """
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 from pythonbible import *
