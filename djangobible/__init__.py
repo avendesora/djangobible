8ce3fbd7c71d2a7e4f8949f715713a4412c0a22d
Nathan Patton <npatton@gmail.com>
2020-10-29 16:37:57 -0500

    version bump for template tags

--- djangobible/__init__.py
+++ djangobible/__init__.py
@@ -1,9 +1,10 @@
 """
-django-bible python library.
+djangobible python library.
 
-django-bible lets you easily associate Django models with Bible Scripture references and search accordingly.
+djangobible lets you easily associate Django models with Bible Scripture references and search accordingly.
 """
 
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 
+# This import statement allows djangobible to be a wrapper for pythonbible.
 from pythonbible import *
