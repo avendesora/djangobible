34a9f71eed01f6305c3879c1b858a026ac55677e
Nathan Patton <npatton@gmail.com>
2020-11-30 16:45:36 -0600

    version bump for pythonbible

--- djangobible/__init__.py
+++ djangobible/__init__.py
@@ -4,7 +4,7 @@ djangobible python library.
 djangobible lets you easily associate Django models with Bible Scripture references and search accordingly.
 """
 
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 
 # This import statement allows djangobible to be a wrapper for pythonbible.
 from pythonbible import *
