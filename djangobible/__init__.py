9877e74f9864d3c7d300409e8b1be9a1c3cabcf4
Nathan Patton <npatton@gmail.com>
2022-04-29 19:13:16 -0500

    pythonbible 0.7

--- djangobible/__init__.py
+++ djangobible/__init__.py
@@ -4,6 +4,6 @@ djangobible python library.
 djangobible lets you easily associate Django models with Bible Scripture references and search accordingly.
 """
 
-__version__ = "0.0.20"
+__version__ = "0.0.21"
 
 from pythonbible import *
