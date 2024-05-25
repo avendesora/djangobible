1a912648b210e98535f30fbdf23c2723f7a4cae3
Nathan Patton <npatton@gmail.com>
2024-05-24 21:59:26 -0500

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13.0-beta.1"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"])
 @nox.parametrize("django", ["4.2"])
 def tests(session: nox.Session, django: str) -> None:
     """Run the test suite."""
@@ -13,7 +13,7 @@ def tests(session: nox.Session, django: str) -> None:
     session.notify("tests_django_5")
 
 
-@nox.session(python=["3.10", "3.11", "3.12", "3.13.0-beta.1"])
+@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
 @nox.parametrize("django", ["5.0", "5.1a1"])
 def tests_django_5(session: nox.Session, django: str) -> None:
     """Run the test suite for Django 5.0+ (Python 3.10+)."""
